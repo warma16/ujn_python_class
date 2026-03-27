c29d1a6c12404d18bb58fee8d2951357a6297d3d
warma16 <metalstudio@126.com>
2026-03-27 16:50:38 +0800

    fix:添加gitignore

---
+++ section3/bool_learning.py
@@ -0,0 +1,7 @@
+a=5
+b=10
+result1=(a<b)
+c=20
+result2=(b>c)
+print(result1 and result2)
+#False
---
+++ section3/condition_judgement.py
@@ -0,0 +1,5 @@
+myWeight=70.5
+yourWeight=81.3
+if myWeight < yourWeight:
+    print("You are heavier than me")
+#输出:You are heavier than me
